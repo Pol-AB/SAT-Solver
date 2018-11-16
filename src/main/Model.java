9bb1171f7a25c37efb995f8a5ec334f82e2241a1
PAB <polaurelien.boidot@gmail.com>
2018-11-16 13:28:12 +0100

    Changement de Modele -> générer modèles devient tester modèles (best case largement amélioré, worst case identique)

--- src/main/Model.java
+++ src/main/Model.java
@@ -42,29 +42,34 @@ public class Model {
     }
 
     /**
-     * Renvoie la liste de tous les modeles de varNum variables
-     * @param varNum nombre de variables des modeles a generer
-     * @return la liste des modeles crees
+     * Renvoie un modele de cnf si il en existe un
+     * @param cnf la formule a tester
+     * @return le modlele, si on en a trouve un
      */
-    public static ArrayList<Model> generateAllModels(int varNum){
-        ArrayList<Model> ret = new ArrayList<>();
+    public static Model testAllModels(Formula cnf){
 
-        for(Integer j = 0; j < (Math.pow(2, varNum)) ; j++){
+        for(Integer j = 0; j < (Math.pow(2, cnf.getNbVar())) ; j++){
+            //to create a model we need nothing more than a unique list of 0 and 1
             ArrayList<Integer> temp = new ArrayList<>();
-            for(int i = 0 ; i < varNum ; i++){
+            for(int i = 0 ; i < cnf.getNbVar() ; i++){
                 temp.add(i,0);
             }
             String model = Integer.toBinaryString(j);
 
-            while(model.length() < varNum){
+            while(model.length() < cnf.getNbVar()){
                 model = "0" + model;
             }
 
             for(int k = 0 ; k < model.length() ; k++){
                 temp.set(k, Character.getNumericValue(model.charAt(k)));
             }
-            ret.add(new Model(temp));
+
+            Model m = new Model(temp);      //we create a brand new model with that list
+
+            if(cnf.isTrue(m)){
+                return m;       //if this model works for our cnf, we return it and the algorithm ends there (else we keep searching)
+            }
         }
-        return ret;
+        return null;        //if we found no suitable model, we return null
     }
 }
